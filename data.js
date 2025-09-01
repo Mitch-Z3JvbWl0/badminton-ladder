100dbe7133d92fa33217d4078ca3ae433047981e
Mitch <mitchellhart@live.co.uk>
2025-09-01 12:28:14 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,41 @@
+// Seasoned fixtures
+const fixturesBySeason = {
+  1: [
+    {A:"Will",B:"Luke",Ascore:14,Bscore:16,Winner:"Luke",Week:"Week 1"},
+    {A:"James",B:"Mitch",Ascore:6,Bscore:11,Winner:"Mitch",Week:"Week 1"},
+    {A:"Jordan",B:"Will",Ascore:11,Bscore:5,Winner:"Jordan",Week:"Week 1"},
+    {A:"Luke",B:"James",Ascore:5,Bscore:11,Winner:"James",Week:"Week 1"},
+    {A:"Mitch",B:"Jordan",Ascore:11,Bscore:9,Winner:"Mitch",Week:"Week 1"},
+    {A:"Will",B:"James",Ascore:7,Bscore:11,Winner:"James",Week:"Week 1"},
+    {A:"Jordan",B:"Luke",Ascore:11,Bscore:8,Winner:"Jordan",Week:"Week 1"},
+    {A:"Mitch",B:"Will",Ascore:11,Bscore:8,Winner:"Mitch",Week:"Week 1"},
+    {A:"James",B:"Jordan",Ascore:3,Bscore:11,Winner:"Jordan",Week:"Week 1"},
+    {A:"Luke",B:"Mitch",Ascore:9,Bscore:11,Winner:"Mitch",Week:"Week 1"},
+    {A:"Will",B:"Kepp",Ascore:21,Bscore:18,Winner:"Will",Week:"Week 2"},
+    {A:"Luke",B:"Jordan",Ascore:12,Bscore:21,Winner:"Jordan",Week:"Week 2"},
+    {A:"James",B:"Mitch",Ascore:18,Bscore:21,Winner:"Mitch",Week:"Week 2"},
+    {A:"Kepp",B:"Luke",Ascore:19,Bscore:21,Winner:"Luke",Week:"Week 2"},
+    {A:"Will",B:"Jordan",Ascore:12,Bscore:21,Winner:"Jordan",Week:"Week 2"},
+    {A:"James",B:"Kepp",Ascore:21,Bscore:16,Winner:"James",Week:"Week 2"},
+    {A:"Mitch",B:"Luke",Ascore:21,Bscore:10,Winner:"Mitch",Week:"Week 2"},
+    {A:"Jordan",B:"Kepp",Ascore:21,Bscore:8,Winner:"Jordan",Week:"Week 2"},
+    {A:"Will",B:"James",Ascore:17,Bscore:21,Winner:"James",Week:"Week 2"},
+    {A:"Mitch",B:"Jordan",Ascore:21,Bscore:14,Winner:"Mitch",Week:"Week 2"},
+    {A:"Luke",B:"James",Ascore:22,Bscore:24,Winner:"James",Week:"Week 2"},
+    {A:"Will",B:"Mitch",Ascore:17,Bscore:21,Winner:"Mitch",Week:"Week 2"},
+    {A:"Jordan",B:"James",Ascore:21,Bscore:17,Winner:"Jordan",Week:"Week 2"},
+    {A:"Kepp",B:"Mitch",Ascore:15,Bscore:21,Winner:"Mitch",Week:"Week 2"},
+    {A:"Will",B:"Luke",Ascore:18,Bscore:21,Winner:"Luke",Week:"Week 2"}
+  ],
+  2: [] // future
+};
+
+// Player profiles
+const playerProfiles = {
+  "Mitch": {hand:"Right",height:"1.80m",age:32,racket:"Yonex Astrox 88D"},
+  "Luke": {hand:"Right",height:"1.82m",age:28,racket:"Yonex Nanoflare 700"},
+  "James": {hand:"Left",height:"1.76m",age:31,racket:"Victor Thruster K"},
+  "Will": {hand:"Right",height:"1.85m",age:30,racket:"Yonex Duora 10"},
+  "Jordan": {hand:"Right",height:"1.78m",age:29,racket:"Li-Ning Aeronaut"},
+  "Kepp": {hand:"Right",height:"1.80m",age:27,racket:"Yonex Voltric Z-Force II"}
+};
